356753e9b5568666bf7c8362a81eb18e369289e0
jessba2 <jessba2@pdx.edu>
2021-10-16 12:07:51 -0700

    with more runs

--- 5armedBandit.py
+++ 5armedBandit.py
@@ -3,6 +3,7 @@
 
 import numpy
 from numpy import random
+import matplotlib.pyplot as plt
 
 print("This program learns to pull the best lever!")
 random.seed()
@@ -11,31 +12,61 @@ def bandit(leverNum):
     reward = random.normal(loc=meanMatrix[leverNum], scale=1, size=None)
     return reward
 
-outerLoop=0
-sumOf200rewardSums = 0
-while (outerLoop <= 199):
-    outerLoop = outerLoop + 1
-    counter=0
-    rewardSum=0
-    #set meanMatrix for levers
-    meanMatrix = numpy.random.randint(-5,5,size=5)
+rewardMatrixPt4 = numpy.zeros((1000, 2000))
+rewardMatrixPt1 = numpy.zeros((1000, 2000))
+rewardMatrix0 = numpy.zeros((1000, 2000))
+epsilonMatrix = [0, 0.02, 0.4]
+differentEpsilonValues = 0
+while(differentEpsilonValues <= 2):
+    trial=0
+    sumOf200rewardSums = 0
+    while (trial <= 1999):
+        timestep=0
+        rewardSum=0
+        #set meanMatrix for levers
+        meanMatrix = numpy.random.randint(-5,5,size=5)
 
-    #Initialize, for a=1 to 5, Q(a) = 0 and N(a)=0
-    NumTimesMatrix = [0, 0, 0, 0, 0]
-    QValueMatrix = [0, 0, 0, 0, 0]
-    while (counter<=99):
-        counter = counter + 1
-        epsilon = .4
-        randomDec = random.random()
-        if randomDec >= epsilon:
-            A = numpy.argmax(QValueMatrix)
-        else:
-            A = random.randint(0, 5)
-        reward = bandit(A)
-        rewardSum = rewardSum + reward
-        NumTimesMatrix[A] = 1 + NumTimesMatrix[A]
-        newValue = QValueMatrix[A] + ((1/NumTimesMatrix[A])*(reward-QValueMatrix[A]))
-        QValueMatrix[A] = newValue
-    sumOf200rewardSums = sumOf200rewardSums + rewardSum
-print("With epsilon value ", epsilon)
-print("the average reward is ", sumOf200rewardSums/20000)
+        #Initialize, for a=1 to 5, Q(a) = 0 and N(a)=0
+        NumTimesMatrix = [0, 0, 0, 0, 0]
+        QValueMatrix = [0, 0, 0, 0, 0]
+        while (timestep <= 999):
+            epsilon = epsilonMatrix[differentEpsilonValues]
+            randomDec = random.random()
+            if randomDec >= epsilon:
+                A = numpy.argmax(QValueMatrix)
+            else:
+                A = random.randint(0, 5)
+            reward = bandit(A)
+            if (epsilon == 0):
+                rewardMatrix0[timestep][trial] = reward
+            elif (epsilon == .4):
+                rewardMatrixPt4[timestep][trial] = reward
+            elif (epsilon == .02):
+                rewardMatrixPt1[timestep][trial] = reward
+            else:
+                print("ut oh")
+            NumTimesMatrix[A] = 1 + NumTimesMatrix[A]
+            newValue = QValueMatrix[A] + ((1/NumTimesMatrix[A])*(reward-QValueMatrix[A]))
+            QValueMatrix[A] = newValue
+            timestep = timestep + 1
+        sumOf200rewardSums = sumOf200rewardSums + rewardSum
+        trial = trial + 1
+    differentEpsilonValues = differentEpsilonValues + 1
+
+averagesMatrixPt4 = numpy.mean(rewardMatrixPt4, axis=1)
+averagesMatrixPt1 = numpy.mean(rewardMatrixPt1, axis=1)
+averagesMatrix0 = numpy.mean(rewardMatrix0, axis=1)
+v=averagesMatrixPt4
+z=averagesMatrixPt1
+w=averagesMatrix0
+x=numpy.arange(0, 1000)
+#print("With epsilon value ", epsilon)
+#print("the average reward is ", sumOf200rewardSums/20000)
+plt.title("Average Reward by Timestep")
+plt.xlabel("Steps")
+plt.ylabel("Average Reward")
+plt.plot(x, w, "red", label = '0 (greedy)')
+plt.plot(x, z, "blue", label = '0.02')
+plt.plot(x, v, "green", label = '0.4')
+plt.legend(title='Epsilon value')
+plt.show()
